db3271aeb1542a107c4c8652771ea3b7ccae258f
Stephen Pallen <steve.pallen@emetrotel.com>
2023-08-09 19:22:43 -0400

    neovide config

--- options.lua
+++ options.lua
@@ -16,6 +16,9 @@ return {
     diagnostics_mode = 3, -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
     icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
     ui_notifications_enabled = true, -- disable notifications when toggling UI elements
+    neovide_hide_mouse_when_typing = true,
+    neovide_confirm_quit = true,
+    neovide_scroll_animation_length = 0.5,
   },
 }
 -- If you need more control, you can use the function()...end notation
